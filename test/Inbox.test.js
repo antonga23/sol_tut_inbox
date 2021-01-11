78305f88083164b3b001114733dea8158167035c
alatha <alathantonga@gmail.com>
2021-01-11 19:00:09 +0200

    more tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,10 +3,12 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const {interface, bytecode} = require('../compile');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 let accounts;
 let inbox;
+
 beforeEach(async () => {
     //Get a list of all accounts
     accounts = await web3.eth.getAccounts()
@@ -19,12 +21,24 @@ beforeEach(async () => {
         .deploy({data: bytecode, arguments: ['Hi there!']})
         .send({from: accounts[0], gas: 1000000});
 
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () =>{
     it('deploys a contact', () => {
-        assert.ok(inbox.options.address);
+        assert.ok(inbox.options.address); // assert that contacrt has been deployed and has an address
     });
+
+    it('has a default message', async () =>{
+        const message = await inbox.methods.message().call(); // since message() is not a transaction on the chain call() is empty
+        assert.equal(message, 'Hi there!');
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    })
 });
 
 
